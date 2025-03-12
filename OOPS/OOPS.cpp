e9e62e3a6c01c9b63d9889655f6714246531d2b9
Mohitkmr141 <Mohitkmr3621@gmail.com>
2025-03-12 13:22:13 +0530

    fix problems in repo

--- OOPS/OOPS.cpp
+++ OOPS/OOPS.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
 using namespace std;
+class Node{
+
+    public:
+    int data;
+    Node *next;
+
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+
+    }
+};
 int main(){
 
 
+Node *a = new Node(1);
+Node *b = new Node(2);
+Node *c = new Node(3);
 
-
-
+cout<<a->next->data<<endl;
 
 
     return 0;
